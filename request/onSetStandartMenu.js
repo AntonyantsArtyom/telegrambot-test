b9efb968e0db4be48562f7e91fdbb2e4bb171028
AntonyantsArtyom <aa.artem.0303@mail.ru>
2023-06-27 06:49:49 +0300

    fixcaffemenu

--- request/onSetStandartMenu.js
+++ request/onSetStandartMenu.js
@@ -9,23 +9,17 @@ const onSetStandartMenu = async (bot, msg) => {
             responseType: "arraybuffer",
          })
          .then((response) => Buffer.from(response.data, "binary"))
-      await bot.editMessageMedia(
-         {
-            type: "photo",
-            media: image,
+      await bot.deleteMessage(msg.message.chat.id, msg.message.message_id)
+      await bot.sendPhoto(msg.message.chat.id, image, {
+         parse_mode: "HTML",
+         reply_markup: {
+            inline_keyboard: [
+               [{ text: "стандартное меню ✅", callback_data: "menu_standart" }],
+               [{ text: "летнее меню", callback_data: "menu_summer" }],
+               [{ text: "назад в меню", callback_data: "back_to_menu" }],
+            ],
          },
-         {
-            chat_id: msg.message.chat.id,
-            message_id: msg.message.message_id,
-            reply_markup: {
-               inline_keyboard: [
-                  [{ text: "стандартное меню ✅", callback_data: "menu_standart" }],
-                  [{ text: "летнее меню", callback_data: "menu_summer" }],
-                  [{ text: "назад в меню", callback_data: "back_to_menu" }],
-               ],
-            },
-         }
-      )
+      })
    } catch (error) {
       console.log(error)
    }
--- request/onSetSummerMenu.js
+++ request/onSetSummerMenu.js
@@ -4,27 +4,21 @@ process.env["NTBA_FIX_350"] = 1
 const onSetSummerMenu = async (bot, msg) => {
    try {
       const image = await axios
-         .get(`${process.env.SERVER_URL}/standartmenu.jpg`, {
+         .get(`${process.env.SERVER_URL}/summermenu.jpg`, {
             responseType: "arraybuffer",
          })
          .then((response) => Buffer.from(response.data, "binary"))
-      await bot.editMessageMedia(
-         {
-            type: "photo",
-            media: image,
+      await bot.deleteMessage(msg.message.chat.id, msg.message.message_id)
+      await bot.sendPhoto(msg.message.chat.id, image, {
+         parse_mode: "HTML",
+         reply_markup: {
+            inline_keyboard: [
+               [{ text: "стандартное меню", callback_data: "menu_standart" }],
+               [{ text: "летнее меню ✅", callback_data: "menu_summer" }],
+               [{ text: "назад в меню", callback_data: "back_to_menu" }],
+            ],
          },
-         {
-            chat_id: msg.message.chat.id,
-            message_id: msg.message.message_id,
-            reply_markup: {
-               inline_keyboard: [
-                  [{ text: "стандартное меню", callback_data: "menu_standart" }],
-                  [{ text: "летнее меню ✅", callback_data: "menu_summer" }],
-                  [{ text: "назад в меню", callback_data: "back_to_menu" }],
-               ],
-            },
-         }
-      )
+      })
    } catch (error) {
       console.log(error)
    }
